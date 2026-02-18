9c50ad43c37609827bf16f3601e861798aeca33b
Aymir Sunchugashev <as4n.tsu@gmail.com>
2026-02-18 14:05:52 +0700

    inital commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,26 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+    tailwindcss(),
+  ],
+  server: {
+    host: "localhost",
+    port: 5500,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
